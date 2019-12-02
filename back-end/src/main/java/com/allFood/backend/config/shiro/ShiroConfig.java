6f606a3018439223a5a88d94f7523433a47b7253
FlyingCloud-Genius <39827741+FlyingCloud-Genius@users.noreply.github.com>
2019-12-02 19:07:08 +0800

    new changes

--- back-end/src/main/java/com/allFood/backend/config/shiro/ShiroConfig.java
+++ back-end/src/main/java/com/allFood/backend/config/shiro/ShiroConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
-
 import javax.servlet.Filter;
 import java.util.HashMap;
 import java.util.Map;
